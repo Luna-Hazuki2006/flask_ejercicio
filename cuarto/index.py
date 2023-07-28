750de2b0f95e79b51eb2afc7c0955198a0aa00d8
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-07-28 16:54:20 -0400

    cositas

---
+++ cuarto/index.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, redirect,  url_for, flash
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/tareas")
+def tareas():
+    tareas = [
+        "Tocar la flauta", 
+        "Mirar al techo", 
+        "Responder los mensajes"
+    ]
+    return render_template('./tareas/index.html', tareas=tareas)
+
+@app.route("/acerca")
+def acerca():
+    return render_template("./acerca/index.html")
+
+mensajes_salvados = []
+
+@app.route("/mensajes", methods=['GET', 'POST'])
+def crear_mensaje():
+    print(request.method)
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        contenido = request.form['contenido']
+
+        if not titulo:
+            flash("El título es obligatorio")
+        if not contenido:
+            flash('El contenido es obligatorio')
+
+        if titulo and contenido:
+            mensajes_salvados.append({'titulo': titulo, 'contenido': contenido})
+            return redirect(url_for('mensajes'))
+    return render_template("./crear_mensajes/index.html")
+
+@app.route("/ver_mensajes")
+def mensajes():
+    return render_template("./mensajes/index.html", mensajes=mensajes_salvados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
