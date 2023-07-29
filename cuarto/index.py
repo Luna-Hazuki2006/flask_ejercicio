57a72ab1b79890ecfa8adc9f3c9c216e88076ed2
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-07-29 17:11:26 -0400

    cositas

--- cuarto/index.py
+++ cuarto/index.py
@@ -1,30 +1,47 @@
 from flask import Flask, request, render_template, redirect,  url_for, flash
 
 app = Flask(__name__, template_folder="templates")
+app.config['SECRET_KEY'] = '2x^.dcT-ZgUtB:7%@5.y'
 
 @app.route("/contactos")
 def contactos():
     return render_template('./base/index.html')
 
+lista_contactos = []
+
 @app.route("/lista_contactos")
 def listar():
-    return render_template("./acerca/index.html")
+    return render_template("./listar/index.html")
 
 @app.route("/agregar_contacto", methods=['GET', 'POST'])
 def agregar():
     print(request.method)
     if request.method == 'POST':
-        titulo = request.form['titulo']
-        contenido = request.form['contenido']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        telefono = request.form['telefono']
+        correo = request.form['correo']
+        direccion = request.form['direccion']
 
-        if not titulo:
-            flash("El título es obligatorio")
-        if not contenido:
-            flash('El contenido es obligatorio')
+        if not nombre:
+            flash("El nombre es obligatorio")
+        if not apellido:
+            flash('El apellido es obligatorio')
+        if not telefono:
+            flash('El teléfono es obligatorio')
+        if not correo:
+            flash('El correo es obligatorio')
+        if not direccion:
+            flash('La dirección obligatoria')
 
-        if titulo and contenido:
-            return redirect(url_for('mensajes'))
-    return render_template("./crear_mensajes/index.html")
+        if nombre and apellido and telefono and correo and direccion:
+            lista_contactos.append({
+                'nombre': nombre, 'apellido': apellido, 
+                'telefono': telefono, 'correo': correo, 
+                'direccion': direccion})
+            print(lista_contactos)
+            return redirect(url_for('listar'))
+    return render_template("./agregar/index.html")
 
 @app.route("/contacto")
 def ver():
